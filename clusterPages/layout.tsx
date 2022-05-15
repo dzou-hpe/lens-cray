37db4ba36f0fb6dd6dbe56750f3ed2ebf876b822
Di Zou <di.zou@hpe.com>
2022-05-15 15:10:56 -0400

    wip, nodes are rednered

--- clusterPages/layout.tsx
+++ clusterPages/layout.tsx
@@ -1,72 +1,102 @@
 import { Renderer } from "@k8slens/extensions";
-import { makeAutoObservable } from "mobx";
-import { Observer } from "mobx-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ForceGraph2D from "react-force-graph-2d";
-
-class SlsStore {
-  nodes: any[] = [];
-  links: any[] = [];
-  extension: Renderer.LensExtension;
-
-  constructor(extension: Renderer.LensExtension) {
-    makeAutoObservable(this);
-    this.extension = extension;
-    this.loadData();
-  }
-
-  async loadData() {
-    const apiPath = `/api-kube/api/v1/namespaces/services/services/http:cray-sls:80/proxy/v1/hardware`;
-    const clusterId = Renderer.Catalog.catalogEntities.activeEntity.getId();
-    const apiUrl = `http://${clusterId}.localhost:${location.port}`;
-
-    const res = await fetch(apiUrl + apiPath, {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const resJson = await res.json();
-    let n = 0;
-    resJson.forEach((obj: any) => {
-      this.nodes.push({
-        id: n,
-        // value: obj,
-        // group: obj.Type,
-      });
-      n += 1;
-      // this.links.push({
-      //   source: obj.Xname,
-      //   target: obj.Parent,
-      // });
-    });
-    console.log(
-      JSON.stringify({
-        nodes: this.nodes,
-        links: this.links,
-      })
-    );
-  }
-}
 
 // Build a "user interface" that uses the observable state.
 export const LayoutPage = (props: any) => {
   const { extension } = props;
-  const slsStore = new SlsStore(extension);
+  const [state, setState] = useState({ nodes: [], links: [] });
+  useEffect(() => {
+    const apiPath = `/api-kube/api/v1/namespaces/services/services/http:cray-sls:80/proxy/v1/hardware`;
+    const clusterId = Renderer.Catalog.catalogEntities.activeEntity.getId();
+    const apiUrl = `http://${clusterId}.localhost:${location.port}`;
+
+    fetch(apiUrl + apiPath, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      const nodes: any[] = [];
+      const links: any[] = [];
+      res.json().then((resJson) => {
+        // add nodes
+        resJson.forEach((obj: any) => {
+          nodes.push({
+            id: obj.Xname,
+            group:
+              obj.Type + "-" + obj.ExtraProperties?.Role ||
+              "" + "-" + obj.ExtraProperties?.SubRole ||
+              "" + "-" + obj.ExtraProperties?.Brand ||
+              "",
+            value: obj,
+          });
+        });
+
+        // add links
+        resJson.forEach((obj: any) => {
+          if (nodes.findIndex((node) => node.Xname === obj.Parent) === -1) {
+            nodes.push({
+              id: obj.Parent,
+              group: "missing",
+            });
+          }
+          links.push({
+            source: obj.Xname,
+            target: obj.Parent,
+          });
+        });
+
+        setState({ nodes, links });
+      });
+    });
+  }, []);
+
+  const sidebarWidth =
+    (
+      document.querySelectorAll(
+        '[data-testid="cluster-sidebar"]'
+      )[0] as HTMLElement
+    )?.offsetWidth || 200;
+  const sidebarHeight =
+    (
+      document.querySelectorAll(
+        '[data-testid="cluster-sidebar"]'
+      )[0] as HTMLElement
+    )?.offsetHeight || 200;
   return (
-    <Observer>
-      {() => (
-        <div>
-          <ForceGraph2D
-            graphData={{
-              nodes: slsStore.nodes,
-              links: [{ source: 1, target: 3 }],
-            }}
-            width={400}
-            height={600}
-          />
-        </div>
-      )}
-    </Observer>
+    <ForceGraph2D
+      graphData={state}
+      width={window.innerWidth - 70 - sidebarWidth}
+      height={sidebarHeight - 100}
+      linkWidth={(link) => 4}
+      nodeAutoColorBy="group"
+      backgroundColor="white"
+      nodeCanvasObject={(node: any, ctx, globalScale) => {
+        const label: string =
+          node?.value?.Type || "" + ` (${node.id as string})`;
+        const fontSize = 24 / globalScale;
+
+        const r = 1.3;
+
+        if (node.value) {
+          ctx.beginPath();
+          ctx.lineWidth = 2;
+          ctx.arc(node.x, node.y, r + 3, 0, 2 * Math.PI, false);
+          ctx.strokeStyle = node.color;
+          ctx.stroke();
+          ctx.fillStyle = node.color;
+          ctx.fill();
+          ctx.closePath();
+        }
+
+        // draw label
+        ctx.textAlign = "center";
+        ctx.font = `${fontSize}px Arial`;
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = node.color;
+        ctx.fillText(label, node.x, node.y + r + 10 / globalScale);
+      }}
+    />
   );
 };
